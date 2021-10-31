ed25c24a571c5990226aa965c6e76a15100db7cb
PHClement <c.delouvencourt@nocturne.app>
2021-10-31 11:59:32 +0100

    Enhance search

--- src/api/npmAPI.js
+++ src/api/npmAPI.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default class NpmAPI {
 
     static getPackages(page){
-        return axios.get('https://api.npms.io/v2/search?q=react-component+not:deprecated,insecure+keywords:-react-native+keywords:-vue+boost-exact:false&from=' + (100 * page) + '&size=' + 100);
+        return axios.get('https://api.npms.io/v2/search?q=react-component+not:deprecated,insecure+keywords:-react-native+keywords:-vue+keywords:-angular+boost-exact:false&from=' + (100 * page) + '&size=' + 100);
     }
 
     static getPackageInfo(name){
@@ -13,7 +13,7 @@ export default class NpmAPI {
     }
 
     static searchPackage(search){
-        return axios.get('https://api.npms.io/v2/search?q=' + search + '+not:deprecated,insecure+keywords:-react-native+keywords:-vue+boost-exact:false&size=' + 100);
+        return axios.get('https://api.npms.io/v2/search?q=' + search + '+not:deprecated,insecure+keywords:-react-native+keywords:-vue+keywords:-angular+boost-exact:false&size=' + 100);
     }
 
 }
